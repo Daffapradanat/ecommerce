a4f1152d8afcb103212abe1325d8710c15bd2e9a
Daffa pradana <daffapradanatsaqif@gmail.com>
2024-08-21 10:17:07 +0700

    fix update Order notification

--- app/Notifications/NewBuyerOrderNotification.php
+++ app/Notifications/NewBuyerOrderNotification.php
@@ -41,7 +41,7 @@ class NewBuyerOrderNotification extends Notification
             'message' => $this->order->buyer->name . 'order has been placed successfully. ' . $this->order->order_id,
             'order_id' => $this->order->order_id,
             'total_price' => $this->order->total_price,
-            'url' => '/orders/' . $this->order->order_id
+            'url' => route('order.show', ['order' => $this->order->id])
         ];
     }
 }
--- app/Notifications/NewOrderNotification.php
+++ app/Notifications/NewOrderNotification.php
@@ -42,3 +42,5 @@ class NewOrderNotification extends Notification
         ];
     }
 }
+
+// TIDAK KEPAKAI
