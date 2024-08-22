27d593b3a60366ee1ef0079cb8e0dad3d8ca75ef
Daffa pradana <daffapradanatsaqif@gmail.com>
2024-08-22 15:30:48 +0700

    update view for verify email

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,29 +1,34 @@
 @extends('layouts')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Verify Your Email Address') }}</div>
 
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address. Please check your email and click on the verification link to complete the process.') }}
-                        </div>
-                    @endif
+                    <div class="card-body">
+                        @if (session('resent'))
+                            <div class="alert alert-success" role="alert">
+                                {{ __('A fresh verification link has been sent to your email address. Please check your email and click on the verification link to complete the process.') }}
+                            </div>
+                        @endif
 
-                    {{ __('If you have already verified your email, please') }} <a href="{{ route('login') }}">{{ __('login here') }}</a>.
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
-                    <form class="d-inline" method="POST" action="{{ route('verification.send') }}">
-                        @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
-                    </form>
+                        {{ __('Before proceeding, please check your email for a verification link.') }}
+                        {{ __('If you did not receive the email') }},
+                        <form class="d-inline" method="POST" action="{{ route('verification.send') }}">
+                            @csrf
+                            <button type="submit"
+                                class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        </form>
+
+                        <hr>
+
+                        {{ __('If you have already verified your email, please') }} <a
+                            href="{{ route('login') }}">{{ __('login here') }}</a>.
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
